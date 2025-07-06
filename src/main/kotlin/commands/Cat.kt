fc9ad7d92d2b6b24dcb66c9e0959c68329bfd79c
Ahmed Hassan <ahmedhosam.1591@gmail.com>
2025-07-06 18:01:41 +0200

    REfactor cat

--- src/main/kotlin/commands/Cat.kt
+++ src/main/kotlin/commands/Cat.kt
@@ -8,24 +8,21 @@ fun cat(option: String, hash: String): Result<String> {
     if (option != "-p") {
         return Result.failure(IllegalArgumentException("Unknown command!"))
     }
-
-    val objectsDir = File(Constants.OBJECTS_PATH.toString())
-    val dir = File(objectsDir, hash.take(2))
-    val file = File(dir, hash.drop(2))
+    val file = Constants.OBJECTS_PATH
+        .resolve(hash.take(2))
+        .resolve(hash.drop(2)).toFile()
 
     if (!file.exists()) {
         return Result.failure(FileNotFoundException("File not found: ${file.path}"))
     }
 
     return runCatching {
-        val inflater = InflaterInputStream(file.inputStream())
-        val decompressed = inflater.reader().readText()
-        inflater.close()
-
-        val content = decompressed.substringAfter('\u0000', missingDelimiterValue = "")
-        if (content.isEmpty()) {
-            throw IllegalStateException("Null character not found in the object content")
+            InflaterInputStream(file.inputStream()).use { inflater ->
+            val decompressed = inflater.reader().readText()
+            val content = decompressed.substringAfter('\u0000', missingDelimiterValue = "")
+            content.takeUnless {it.isEmpty()}?:
+            throw IllegalStateException("Null charachter not found in the object content")
         }
-        content
+
     }
 }
